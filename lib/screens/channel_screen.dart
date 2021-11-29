3dd43e9e708cb597c54b693feb58263eff3cc238
lecheliza <lechoosz@gmail.com>
2021-11-29 15:55:20 +0100

    small improvements in UI

--- lib/screens/channel_screen.dart
+++ lib/screens/channel_screen.dart
@@ -12,10 +12,18 @@ class _ChannelScreenState extends State<ChannelScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: const Color(0xFF1A1A1A),
+        foregroundColor: Colors.pinkAccent,
+        backgroundColor: const Color(0x221A1A1A),
         elevation: 0.0,
         leading: null,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_horiz),
+          ),
+        ],
         title: Text(
           'Name of the channel',
           style: TextStyle(
@@ -24,11 +32,51 @@ class _ChannelScreenState extends State<ChannelScreen> {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text('hello to channel\'s screen')),
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  children: [
+                    Text('photo maybe?'),
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.group),
+                      style: ButtonStyle(
+                        //foregroundColor: MaterialStateProperty.all<Color>(Colors.pinkAccent),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.pinkAccent),
+                      ),
+                      onPressed: () {},
+                      label: Text('Join'),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    Text(
+                      'Description',
+                      style: kBannerTextStyle,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                              'owner\'s description about sport and people they are looking for'),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      'About this channel',
+                      style: kBannerTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
