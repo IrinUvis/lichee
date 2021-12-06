5e0750175a8388ddfc536e628f7ab06eee01bee5
Arsemi337 <arturmilosz@onet.pl>
2021-12-06 19:54:26 +0100

    dialog with wilters
    based on package content

--- lib/screens/channel_list_screen.dart
+++ lib/screens/channel_list_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_treeview/flutter_treeview.dart';
+import 'package:filter_list/filter_list.dart';
+import 'package:lichee/constants.dart';
 
 class ChannelListScreen extends StatefulWidget {
-  ChannelListScreen({Key? key}) : super(key: key);
+  const ChannelListScreen({Key? key}) : super(key: key);
 
   @override
   _ChannelListScreenState createState() => _ChannelListScreenState();
@@ -11,8 +14,127 @@ class _ChannelListScreenState extends State<ChannelListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Channel list Screen'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              const Center(
+                child: Text(
+                  'Channels',
+                  style: kLicheeTextStyle,
+                ),
+              ),
+              LicheeButton(
+                text: 'Filters',
+                callback: _openFilterDialog,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<String> filtersList = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten'
+  ];
+  List<String>? selectedFiltersList = [];
+
+  void _openFilterDialog() async {
+    await FilterListDialog.display<String>(
+      context,
+      listData: filtersList,
+      selectedListData: selectedFiltersList,
+      backgroundColor: const Color(0xFF1A1A1A),
+      searchFieldBackgroundColor: Colors.pinkAccent,
+      selectedTextBackgroundColor: Colors.pinkAccent,
+      applyButonTextBackgroundColor: Colors.pinkAccent,
+      closeIconColor: Colors.white,
+      headerTextColor: Colors.white,
+      applyButtonTextStyle: const TextStyle(color: Colors.white),
+      searchFieldTextStyle: const TextStyle(color: Colors.white),
+      controlButtonTextStyle: const TextStyle(color: Colors.black),
+      headlineText: 'Select Filters',
+      searchFieldHintText: 'Search Here',
+      selectedItemsText: 'selected filters',
+      choiceChipLabel: (item) {
+        return item;
+      },
+      validateSelectedItem: (list, val) {
+        return list!.contains(val);
+      },
+      onItemSearch: (list, text) {
+        if (list != null) {
+          if (list.any((element) =>
+              element.toLowerCase().contains(text.toLowerCase()))) {
+            /// return list which contains matches
+            return list
+                .where((element) =>
+                    element.toLowerCase().contains(text.toLowerCase()))
+                .toList();
+          }
+        }
+        return [];
+      },
+
+      onApplyButtonClick: (list) {
+        setState(() {
+          selectedFiltersList = List.from(list!);
+        });
+        Navigator.pop(context);
+      },
+    );
+  }
+}
+
+class LicheeButton extends StatelessWidget {
+  const LicheeButton({
+    Key? key,
+    required this.text,
+    required this.callback,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 60,
+        margin: const EdgeInsets.all(15),
+        child: ElevatedButton(
+          onPressed: callback,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.pinkAccent),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width,
+              minHeight: MediaQuery.of(context).size.height / 16,
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              text,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
       ),
     );
   }
